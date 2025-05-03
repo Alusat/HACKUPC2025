dec52067d7dc634b858a475f4086a550bd370b2d
Adria Cebrian Ruiz <adrisworking@gmail.com>
2025-05-03 18:48:19 +0200

    mantenimiento basico

--- data/users.pl
+++ data/users.pl
@@ -3,16 +3,13 @@
 % All city and vibe names are quoted.
 
 user_preference(user1, ['beach', 'outdoor_adventures']).
-user_preference(user2, ['nightlife_and_entertainment']).
-user_preference(user3, ['great_food']).
-user_preference(user4, ['outdoor_adventures']).
+user_preference(user2, ['underrated_destinations']).
+user_preference(user3, ['nightlife_and_entertainment']).
 
-user_city(user1, 'Barcelona').
-user_city(user2, 'New York John F. Kennedy').
-user_city(user3, 'Beijing Capital').
-user_city(user4, 'Amarillo International').
+user_city(user1, 'Anapolis').
+user_city(user2, 'Chongqing').
+user_city(user3, 'Carmel Fritzsche AAF').
 
-user_dest(user1, 'Candala').
-user_dest(user2, 'Hong Kong Intl').
-user_dest(user3, 'Madrid').
-user_dest(user4, 'Lisbon').
+user_dest(user1, 'Abeche').
+user_dest(user2, 'Cat Cays').
+user_dest(user3, 'Whanganui').
