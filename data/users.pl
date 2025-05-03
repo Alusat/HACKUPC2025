f5899cee95e34e84b633cc04c4b9d739a79347e0
Adria Cebrian Ruiz <adrisworking@gmail.com>
2025-05-03 19:33:33 +0200

    bash y output con lat y lon

--- data/users.pl
+++ data/users.pl
@@ -2,14 +2,14 @@
 % Facts are grouped by predicate.
 % All city and vibe names are quoted.
 
-user_preference(user1, ['beach', 'outdoor_adventures']).
-user_preference(user2, ['underrated_destinations']).
-user_preference(user3, ['nightlife_and_entertainment']).
+user_preference(user1, ['art_and_culture', 'outdoor_adventures', 'underrated_destinations']).
+user_preference(user2, ['art_and_culture', 'beach', 'outdoor_adventures']).
+user_preference(user3, ['underrated_destinations', 'nightlife_and_entertainment', 'art_and_culture']).
 
-user_city(user1, 'Anapolis').
-user_city(user2, 'Chongqing').
-user_city(user3, 'Carmel Fritzsche AAF').
+user_city(user1, 'Barcelona').
+user_city(user2, 'Baramita').
+user_city(user3, 'A Coruña').
 
-user_dest(user1, 'Abeche').
-user_dest(user2, 'Cat Cays').
-user_dest(user3, 'Whanganui').
+user_dest(user1, 'Atbara').
+user_dest(user2, 'Bellary').
+user_dest(user3, 'Aguaclara').
