ed14c6c506665c3ab954315c17fdd08a0671b5e8
Alusat <alusat22@gmail.com>
2025-05-04 00:02:09 +0200

    lololo

--- data/users.pl
+++ data/users.pl
@@ -2,8 +2,8 @@
 % Facts are grouped by predicate.
 % All city and vibe names are quoted.
 
-user_preference(user1, ['underrated_destinations']).
+user_preference(user1, ['great_food']).
 
-user_city(user1, 'Abilene Municipal').
+user_city(user1, 'Barcelona (BCN)').
 
-user_dest(user1, 'Aachen / Merzbruck').
+user_dest(user1, 'Columbia Metropolitan (CAE)').
